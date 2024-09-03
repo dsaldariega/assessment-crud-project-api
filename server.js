73c4b961645b4b36ec3ec441590dc9654302fa89
Dan Christopher Saldariega <dsaldariega@gmail.com>
2024-09-03 21:13:16 +0800

    add cors

--- server.js
+++ server.js
@@ -5,13 +5,46 @@ const mongoose = require("mongoose");
 const postRoute = require("./routes/postRoute.js");
 const userRoute = require("./routes/userRoute.js");
 
-//
 const app = express();
 const PORT = process.env.PORT || 5000; // Define PORT variable
 const dbURL = process.env.DB_URL;
 
 // Middleware for parsing request body
 app.use(express.json());
+
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Use env variable for production or allow all
+  // res.setHeader("Access-Control-Allow-Origin", process.env.URL_PROD || "*");
+  res.setHeader("Access-Control-Allow-Origin", process.env.URL_LOCAL || "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+// Define a wrapper for routes to include CORS
+const wrapWithCors = (route) =>
+  allowCors((req, res, next) => route(req, res, next));
+
+// Example usage: applying CORS middleware to specific route handlers
+app.get(
+  "/",
+  wrapWithCors((req, res) => {
+    console.log(req);
+    return res.status(234).send("Server is working!");
+  })
+);
 
 app.use("/posts", postRoute);
 app.use("/user/auth", userRoute);
